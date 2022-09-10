6d92db027dbf405ff069d48818d915637859d11c
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-09-10 13:43:59 -0500

    Add _at_names property to FebBase

--- fepyio/feb_base.py
+++ fepyio/feb_base.py
@@ -3,10 +3,12 @@ Contains base classes for FEB module
 """
 from dataclasses import dataclass, fields
 from enum import Enum
-from typing import Any
+from typing import Any, Union
 
 from fepyio.utils.dict_utils import prune_dict, unlist_dict
 from fepyio.utils.xmltodict_key_converter import prop_to_xml
+
+AtNames = set[str]
 
 
 @dataclass
@@ -32,8 +34,22 @@ class FebBase:
         """Return key used for class name. Defaults to class name."""
         return self.__class__.__name__
 
+    @property
+    def _at_names(self) -> Union[set[str], set]:
+        """Set of keys that should start with a '@'"""
+        return set()
+
     def _convert_key(self, key: str) -> str:
-        return prop_to_xml(key)
+        return prop_to_xml(key) if key not in self._at_names else f"@{key}"
+
+    def _populate_key_map(self) -> None:
+        # Skip "_key" field name because it is only used for nested FebBase objects
+        keys = [field.name for field in fields(self) if field.name != "_key"]
+
+        self._key_map: dict[str, str] = {key: self._convert_key(key) for key in keys}
+
+    def __post_init__(self):
+        self._populate_key_map()
 
     def to_dict(self) -> dict:
         """Get class as dictionary."""
@@ -48,7 +64,7 @@ class FebBase:
 
             else:
                 # Convert field name to xmltodict-compatible name
-                return (self._convert_key(field.name), value)
+                return (self._key_map[field.name], value)
 
         for field in fields(self):
             # Skip '_key' field
@@ -63,12 +79,12 @@ class FebBase:
 
             if isinstance(field_val, list):
                 # Add each element of list to the dictionary
-                _dict[self._convert_key(field.name)] = [
+                _dict[self._key_map[field.name]] = [
                     _to_dict(value)[1] for value in field_val
                 ]
             elif isinstance(field_val, FebEnum):
                 # Extract enum value
-                _dict[self._convert_key(field.name)] = field_val.get_value()
+                _dict[self._key_map[field.name]] = field_val.get_value()
             else:
                 key, value = _to_dict(field_val)
                 _dict[key] = value
